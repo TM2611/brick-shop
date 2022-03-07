f69e92651c01ae8349449484307e63bef70cf5d4
UP938751 <40968375+UP938751@users.noreply.github.com>
2022-03-07 23:57:15 +0000

    update basket total

--- public/basket.js
+++ public/basket.js
@@ -1,4 +1,4 @@
-// TODO: Save basket item quantity in local storage
+
 
 import fetchProducts from './shop.js';
 /* global localStorage */
@@ -10,36 +10,42 @@ export async function initBasket() {
   const isBasketEmpty = localStorage.getItem('basket') === null;
   if (!isBasketEmpty) {
     basket = JSON.parse(localStorage.getItem('basket'));
-    const basketDOM = document.querySelector('.basket');
     const products = await fetchProducts(); // necessary?
-    const basketQuantity = document.querySelector('.basket-quantity');
+    const basketDOM = document.querySelector('.basket');
+    const basketQuantityDOM = document.querySelector('.basket-quantity');
     const t2 = document.querySelector('#basket-item-template');
-    basketQuantity.innerText = 0;
+    const basketTotalDOM = document.querySelector('.basket-total');
+    let total = 0;
+    basketQuantityDOM.textContent = 0;
     for (const itemID of basket) {
+      const basketQuantity = parseInt(basketQuantityDOM.textContent);
       const itemTemplate = t2.content.cloneNode(true);
-      const basketItem = itemTemplate.querySelector('.basket-item');
-      const img = itemTemplate.querySelector('#basket-img');
-      const productName = itemTemplate.querySelector('#basket-name');
-      const productPrice = itemTemplate.querySelector('#basket-price');
+      const basketItemDOM = itemTemplate.querySelector('.basket-item');
+      const img = itemTemplate.querySelector('#basket-product-img');
+      const productName = itemTemplate.querySelector('#basket-product-name');
+      const productPriceDOM = itemTemplate.querySelector('#basket-product-price');
       const removeItemBtn = itemTemplate.querySelector('.remove-item');
       const increaseBtn = itemTemplate.querySelector('.fa-chevron-up');
       const decreaseBtn = itemTemplate.querySelector('.fa-chevron-down');
-      const itemAmount = itemTemplate.querySelector('.item-amount');
+      const itemAmountDOM = itemTemplate.querySelector('.item-amount');
       const product = products.find(({ id }) => id === itemID);
-      basketItem.dataset.id = product.id; // Set ID in DOM
-      basketQuantity.innerText = parseInt(basketQuantity.innerText) + 1;
-      removeItemBtn.innerText = 'Remove';
+      const price = product.price / 100;
+      basketItemDOM.dataset.id = product.id; // Set ID in DOM
+      basketQuantityDOM.textContent = basketQuantity + 1;
+      removeItemBtn.textContent = 'Remove';
       img.src = `${product.imgSrc}`;
       img.alt = `${product.imgSrc}`;
-      productName.textContent = `${product.name}`;
-      productPrice.textContent = `£${(product.price / 100).toFixed(2)}`;
+      productName.textContent = product.name;
+      productPriceDOM.textContent = price.toFixed(2);
       increaseBtn.addEventListener('click', increaseQuantity);
       decreaseBtn.addEventListener('click', decreaseQuantity);
       removeItemBtn.addEventListener('click', removeBasketItem);
       // TODO: Save basket item quantity in local storage
-      itemAmount.innerText = 1; // = item quantinty from storage
+      itemAmountDOM.textContent = 1; // = item quantinty from storage
       basketDOM.append(itemTemplate);
+      total += price;
     }
+    basketTotalDOM.textContent = total.toFixed(2);
     return;
   }
   basket = [];
@@ -47,16 +53,25 @@ export async function initBasket() {
 
 
 function removeBasketItem(e) {
-  const itemID = parseInt(e.target.parentNode.parentNode.dataset.id);
+  const basketItemDOM = e.target.parentNode.parentNode;
+  const basketTotalDOM = document.querySelector('.basket-total');
+  const basketTotal = parseFloat(basketTotalDOM.textContent);
+  const productPriceDOM = e.target.parentNode.querySelector('#basket-product-price');
+  const price = parseFloat(productPriceDOM.textContent);
+  const itemAmountDOM = basketItemDOM.querySelector('.item-amount');
+  const itemAmount = parseInt(itemAmountDOM.textContent);
+  const itemID = parseInt(basketItemDOM.dataset.id);
   const index = basket.indexOf(itemID);
-  const basketQuantity = document.querySelector('.basket-quantity');
+  const basketQuantityDOM = document.querySelector('.basket-quantity');
+  const basketQuantity = parseInt(basketQuantityDOM.textContent);
   basket.splice(index, 1); // Remove ID from array
   localStorage.setItem('basket', JSON.stringify(basket));
-  e.target.parentElement.parentElement.remove(); // Remove item from DOM
-  basketQuantity.innerText = parseInt(basketQuantity.innerText) - 1;
+  basketItemDOM.remove(); // Remove item from DOM
+  basketQuantityDOM.textContent = basketQuantity - 1;
+  // TODO: bug: removing 3 red bricks (60p) causes negative sign in total
+  basketTotalDOM.textContent = (basketTotal - (price * itemAmount)).toFixed(2);
   resetAddToBasketBtn(itemID);
 }
-
 
 function resetAddToBasketBtn(itemID = 'n/a') {
   const addToBasketList = document.querySelectorAll('.add-to-basket');
@@ -76,20 +91,32 @@ function resetAddToBasketBtn(itemID = 'n/a') {
       }
     }
   }
+  // updateTotalPrice()
 }
 
 function increaseQuantity(e) {
-  const itemAmount = e.target.parentNode.querySelector('.item-amount');
-  itemAmount.innerText = parseInt(itemAmount.innerText) + 1;
+  const itemAmountDOM = e.target.parentNode.querySelector('.item-amount');
+  const itemAmount = parseInt(itemAmountDOM.innerText);
+  const productPriceDOM = e.target.parentNode.parentNode.querySelector('#basket-product-price');
+  const price = parseFloat(productPriceDOM.textContent);
+  const basketTotalDOM = document.querySelector('.basket-total');
+  const basketTotal = parseFloat(basketTotalDOM.textContent);
+  itemAmountDOM.textContent = itemAmount + 1;
+  basketTotalDOM.textContent = (basketTotal + price).toFixed(2);
 }
 
 function decreaseQuantity(e) {
-  const itemAmount = e.target.parentNode.querySelector('.item-amount');
-  if (!(itemAmount.innerText <= 1)) {
-    itemAmount.innerText = parseInt(itemAmount.innerText) - 1;
+  const itemAmountDOM = e.target.parentNode.querySelector('.item-amount');
+  const itemAmount = parseInt(itemAmountDOM.innerText);
+  const productPriceDOM = e.target.parentNode.parentNode.querySelector('#basket-product-price');
+  const price = parseFloat(productPriceDOM.textContent);
+  const basketTotalDOM = document.querySelector('.basket-total');
+  const basketTotal = parseFloat(basketTotalDOM.textContent);
+  if (!(itemAmountDOM.textContent <= 1)) {
+    itemAmountDOM.textContent = itemAmount - 1;
+    basketTotalDOM.textContent = (basketTotal - price).toFixed(2);
   }
 }
-
 
 export async function AddToBasket(e) {
   const itemID = parseInt(e.target.parentNode.dataset.id);
@@ -98,30 +125,34 @@ export async function AddToBasket(e) {
   const product = products.find(({ id }) => id === itemID);
   const t2 = document.querySelector('#basket-item-template');
   const itemTemplate = t2.content.cloneNode(true); // Why is clone necessary ?
-  const basketItem = itemTemplate.querySelector('.basket-item');
-  const img = itemTemplate.querySelector('#basket-img');
-  const productName = itemTemplate.querySelector('#basket-name');
-  const productPrice = itemTemplate.querySelector('#basket-price');
+  const basketItemDOM = itemTemplate.querySelector('.basket-item');
+  const img = itemTemplate.querySelector('#basket-product-img');
+  const productName = itemTemplate.querySelector('#basket-product-name');
+  const productPriceDOM = itemTemplate.querySelector('#basket-product-price');
   const removeItemBtn = itemTemplate.querySelector('.remove-item');
   const increaseBtn = itemTemplate.querySelector('.fa-chevron-up');
   const decreaseBtn = itemTemplate.querySelector('.fa-chevron-down');
   const itemAmount = itemTemplate.querySelector('.item-amount');
   const basketQuantity = document.querySelector('.basket-quantity');
-  basketQuantity.innerText = parseInt(basketQuantity.innerText) + 1;
-  basketItem.dataset.id = itemID; // Set ID in DOM
-  removeItemBtn.innerText = 'Remove';
+  const basketTotalDOM = document.querySelector('.basket-total');
+  const basketTotal = parseFloat(basketTotalDOM.textContent);
+  const price = product.price / 100;
+  basketQuantity.textContent = parseInt(basketQuantity.textContent) + 1;
+  basketItemDOM.dataset.id = itemID; // Set ID in DOM
+  removeItemBtn.textContent = 'Remove';
   img.src = `${product.imgSrc}`;
   img.alt = `${product.imgSrc}`;
-  productName.textContent = `${product.name}`;
-  productPrice.textContent = `£${(product.price / 100).toFixed(2)}`;
+  productName.textContent = product.name;
+  productPriceDOM.textContent = price.toFixed(2);
   increaseBtn.addEventListener('click', increaseQuantity);
   decreaseBtn.addEventListener('click', decreaseQuantity);
   removeItemBtn.addEventListener('click', removeBasketItem);
   basket.push(itemID); // Add ID to basket array
   localStorage.setItem('basket', JSON.stringify(basket));
-  itemAmount.innerText = 1;
-  e.target.innerText = 'In Basket';
+  itemAmount.textContent = 1;
+  e.target.textContent = 'In Basket';
   e.target.disabled = true;
+  basketTotalDOM.textContent = (basketTotal + price).toFixed(2);
   basketDOM.append(itemTemplate);
 }
 
@@ -141,12 +172,14 @@ export function closeBasket() {
 }
 
 export function clearBasket() {
+  const basketTotalDOM = document.querySelector('.basket-total');
   const basketItems = document.querySelectorAll('.basket-item');
   for (const elem of basketItems) {
     elem.remove();
   }
   basket = [];
   localStorage.removeItem('basket');
-  document.querySelector('.basket-quantity').innerText = 0;
+  document.querySelector('.basket-quantity').textContent = 0;
+  basketTotalDOM.textContent = (0).toFixed(2);
   resetAddToBasketBtn();
 }
