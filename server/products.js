7c911466c8152b8f4044ed9f31a065c76326d774
UP938751 <40968375+UP938751@users.noreply.github.com>
2022-03-11 23:32:08 +0000

    added a product + renamed existing products

--- server/products.js
+++ server/products.js
@@ -8,7 +8,7 @@ const products = [
   {
     id: uuid(),
     name: 'Brick 1x1',
-    colour: 'red',
+    colour: 'purple',
     price: 6,
     instock: 1020,
     description:
@@ -17,8 +17,18 @@ const products = [
   },
   {
     id: uuid(),
+    name: 'Brick 1x2',
+    colour: 'purple',
+    price: 10,
+    instock: 333,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.',
+    imgSrc: './images/single/purple/brick1x2.png',
+  },
+  {
+    id: uuid(),
     name: 'Brick 1x4',
-    colour: 'red',
+    colour: 'purple',
     price: 13,
     instock: 723,
     description:
@@ -28,7 +38,7 @@ const products = [
   {
     id: uuid(),
     name: 'Brick 2x2',
-    colour: 'red',
+    colour: 'purple',
     price: 12,
     instock: 877,
     description:
