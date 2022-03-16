5969aabbc4fde960e7fee19181412b01009c5969
UP938751 <40968375+UP938751@users.noreply.github.com>
2022-03-16 23:12:02 +0000

    fixed server error

--- server/server.js
+++ server/server.js
@@ -5,9 +5,13 @@ import url from 'url';
 
 import authConfig from './auth_config.js';
 
+// import * as fil from './filter.js';
+
 import auth0Helpers from './auth0_helper.js';
 
 import { products } from './products.js';
+
+import filterColour from './filter.js';
 
 const app = express();
 
@@ -32,8 +36,16 @@ app.get('/auth_config', (req, res) => {
   res.json(authConfig);
 });
 
+// Filter
+app.get('/products/single/:colour', (req, res) => {
+  const products = filterColour(req.params.colour);
+  debugger
+});
+
 // start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+// TODO: async wrap?
