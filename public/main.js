7136e3668dc3fbc5b0bc3863772a00f6138cdf4a
UP938751 <40968375+UP938751@users.noreply.github.com>
2022-03-31 20:32:28 +0100

    refactored product rendering

--- public/main.js
+++ public/main.js
@@ -31,121 +31,67 @@ export function navbarDisplay() {
   prevScrollpos = currentScrollPos;
 }
 
-// PRODUCTS //
+async function renderProducts() {
+  const item = document.querySelector('.item');
+  if (document.body.contains(item)) {
+    // Products already displayed
+    const productsDOM = document.querySelectorAll('.item');
+    for (const product of productsDOM) {
+      product.remove();
+    }
+  }
+  const products = await fetchFilteredProducts();
+  products.forEach(product => {
+    const t1 = document.querySelector('#product-template');
+    const productTemplate = t1.content.cloneNode(true);
+    const img = productTemplate.querySelector('#product-img');
+    const productName = productTemplate.querySelector('#product-name');
+    const productPrice = productTemplate.querySelector('#product-price');
+    // const productDesc = productTemplate.querySelector('#product-desc');
+    const addToBasket = productTemplate.querySelector('.add-to-basket');
+    const addToBasketBtn = productTemplate.querySelector('.btn-atb');
+    addToBasket.dataset.id = product.ProductID;
+    addToBasketBtn.addEventListener('click', ba.AddToBasket);
+    addToBasketBtn.innerText = 'Add to Basket';
+    if (ba.basket.has(product.ProductID)) {
+      addToBasketBtn.innerText = 'In Basket';
+      addToBasketBtn.disabled = true;
+    }
+    img.src = `${product.ProductImage}`;
+    img.alt = `${product.ProductImage}`;
+    productName.textContent = `${product.ProductName}`;
+    productPrice.textContent = `£${(product.Price / 100).toFixed(2)}`;
+    // productDesc.textContent = `${product.description}`;
+    document.body.append(productTemplate);
+  });
+}
+
+
+async function fetchFilteredProducts() {
+  const filter = document.querySelector('#colour-filter');
+  const colour = filter.options[filter.selectedIndex].text.toLowerCase();
+  const sort = document.querySelector('#sort');
+  const regex = /[)( ]/g ; //Remove any brackets or spaces
+  const sortType = sort.options[sort.selectedIndex].text.replace(regex, '');
+  if (colour === 'any' && sortType === 'MostPopular') {
+    return await fetchAllSingles();
+  }
+  else if (colour !== 'any' && sortType !== 'MostPopular'){ 
+    return await fetchSingleColourSorted(colour, sortType);
+  }
+  //Most popular is default, no sorting needed
+  return await fetchSingleColour(colour)
+}
+
 export default async function fetchAllSingles() {
-  const response = await fetch('/single');
+  const response = await fetch('/single'); // By Most Popular
   if (!response.ok) {
     throw response;
   }
   return response.json();
 }
 
-async function renderProducts() {
-  const item = document.querySelector('.item');
-  if (document.body.contains(item)) {
-    // Products already displayed
-    return;
-  }
-  const products = await fetchAllSingles();
-  products.forEach(product => {
-    const t1 = document.querySelector('#product-template');
-    const productTemplate = t1.content.cloneNode(true);
-    const img = productTemplate.querySelector('#product-img');
-    const productName = productTemplate.querySelector('#product-name');
-    const productPrice = productTemplate.querySelector('#product-price');
-    const addToBasket = productTemplate.querySelector('.add-to-basket');
-    const addToBasketBtn = productTemplate.querySelector('.btn-atb');
-    addToBasket.dataset.id = product.ProductID;
-    addToBasketBtn.addEventListener('click', ba.AddToBasket);
-    addToBasketBtn.innerText = 'Add to Basket';
-    if (ba.basket.has(product.ProductID)) {
-      addToBasketBtn.innerText = 'In Basket';
-      addToBasketBtn.disabled = true;
-    }
-    img.src = `${product.ProductImage}`;
-    img.alt = `${product.ProductImage}`;
-    productName.textContent = `${product.ProductName}`;
-    productPrice.textContent = `£${(product.Price / 100).toFixed(2)}`;
-    document.body.append(productTemplate);
-  });
-}
-
-async function renderFiltered(e) {
-  const item = document.querySelector('.item');
-  if (document.body.contains(item)) {
-    // Products already displayed
-    const productsDOM = document.querySelectorAll('.item');
-    for (const product of productsDOM) {
-      product.remove();
-    }
-  }
-  const products = await fetchFilteredProducts(e);
-  products.forEach(product => {
-    const t1 = document.querySelector('#product-template');
-    const productTemplate = t1.content.cloneNode(true);
-    const img = productTemplate.querySelector('#product-img');
-    const productName = productTemplate.querySelector('#product-name');
-    const productPrice = productTemplate.querySelector('#product-price');
-    // const productDesc = productTemplate.querySelector('#product-desc');
-    const addToBasket = productTemplate.querySelector('.add-to-basket');
-    const addToBasketBtn = productTemplate.querySelector('.btn-atb');
-    addToBasket.dataset.id = product.ProductID;
-    addToBasketBtn.addEventListener('click', ba.AddToBasket);
-    addToBasketBtn.innerText = 'Add to Basket';
-    if (ba.basket.has(product.ProductID)) {
-      addToBasketBtn.innerText = 'In Basket';
-      addToBasketBtn.disabled = true;
-    }
-    img.src = `${product.ProductImage}`;
-    img.alt = `${product.ProductImage}`;
-    productName.textContent = `${product.ProductName}`;
-    productPrice.textContent = `£${(product.Price / 100).toFixed(2)}`;
-    // productDesc.textContent = `${product.description}`;
-    document.body.append(productTemplate);
-  });
-}
-
-async function renderSorted(e) {
-  const item = document.querySelector('.item');
-  if (document.body.contains(item)) {
-    // Products already displayed
-    const productsDOM = document.querySelectorAll('.item');
-    for (const product of productsDOM) {
-      product.remove();
-    }
-  }
-  const products = await fetchSortedProducts(e);
-  products.forEach(product => {
-    const t1 = document.querySelector('#product-template');
-    const productTemplate = t1.content.cloneNode(true);
-    const img = productTemplate.querySelector('#product-img');
-    const productName = productTemplate.querySelector('#product-name');
-    const productPrice = productTemplate.querySelector('#product-price');
-    // const productDesc = productTemplate.querySelector('#product-desc');
-    const addToBasket = productTemplate.querySelector('.add-to-basket');
-    const addToBasketBtn = productTemplate.querySelector('.btn-atb');
-    addToBasket.dataset.id = product.ProductID;
-    addToBasketBtn.addEventListener('click', ba.AddToBasket);
-    addToBasketBtn.innerText = 'Add to Basket';
-    if (ba.basket.has(product.ProductID)) {
-      addToBasketBtn.innerText = 'In Basket';
-      addToBasketBtn.disabled = true;
-    }
-    img.src = `${product.ProductImage}`;
-    img.alt = `${product.ProductImage}`;
-    productName.textContent = `${product.ProductName}`;
-    productPrice.textContent = `£${(product.Price / 100).toFixed(2)}`;
-    // productDesc.textContent = `${product.description}`;
-    document.body.append(productTemplate);
-  });
-}
-
-
-async function fetchFilteredProducts(e) {
-  const colour = (e.target.options[e.target.selectedIndex].text).toLowerCase();
-  if (colour === 'any') {
-    return await fetchAllSingles();
-  }
+async function fetchSingleColour(colour){
   const response = await fetch('/single/colour/' + colour);
   if (!response.ok) {
     throw response;
@@ -153,13 +99,8 @@ async function fetchFilteredProducts(e) {
   return response.json();
 }
 
-async function fetchSortedProducts(e) {
-  const filter = document.querySelector('#colour-filter');
-  const colour = filter.options[filter.selectedIndex].text.toLowerCase();
-  const sdd = document.querySelector('#sort');
-  const regex = /[)( ]/g ; //Remove any brackets or spaces
-  const sort = sdd.options[sdd.selectedIndex].text.replace(regex, '');
-  const response = await fetch(`/single/colour/${colour}/${sort}`);
+async function fetchSingleColourSorted(colour, sortType){
+  const response = await fetch(`/single/colour/${colour}/${sortType}`);
   if (!response.ok) {
     throw response;
   }
@@ -176,8 +117,8 @@ function setupListeners() {
   document.querySelector('.basket-btn').addEventListener('click', ba.viewBasket);
   document.querySelector('.close-basket').addEventListener('click', ba.closeBasket);
   document.querySelector('.clear-basket').addEventListener('click', ba.clearBasket);
-  document.querySelector('#colour-filter').addEventListener('change', renderFiltered);
-  document.querySelector('#sort').addEventListener('change', renderSorted);
+  document.querySelector('#colour-filter').addEventListener('change', renderProducts);
+  document.querySelector('#sort').addEventListener('change', renderProducts);
   window.addEventListener('scroll', navbarDisplay);
   window.addEventListener('click', closeDropdown);
 }
