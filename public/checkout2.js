2104bf3913d99cee93df18167ba266d8addb1a95
UP938751 <40968375+UP938751@users.noreply.github.com>
2022-04-18 17:31:19 +0100

    Added payment fields + refactored rendering on checkout page

--- public/checkout2.js
+++ public/checkout2.js
@@ -5,7 +5,8 @@ import * as fjs from './fetch.js';
 
 
 
-export async function renderCheckout(){
+
+async function renderCheckoutPage(){
   const isBasketEmpty = localStorage.getItem('basket') === null;
   if (isBasketEmpty) {
     const emptyBasketMsg = document.querySelector('#empty-checkout-msg');
@@ -14,10 +15,29 @@ export async function renderCheckout(){
     // continueShopping.innerHTML = '<a href="./index.html">Continue shopping</a>'
     return;
   }
+  await renderReviewSection()
+  // TODO: renderAddressSection()
+  // TODO: renderPaymentMethodSection() ?
+  // const buyNowBtn = document.querySelector('#buy-btn') 
+}
+
+async function renderReviewSection(){
+  document.querySelector('.box-title-basket').textContent = 'Basket';
+  document.querySelector('.box-title-items').textContent = `${calculateItemNumber()} items`;
+  await renderReviewItems()
+
+}
+
+function calculateItemNumber(){
+  let totalQuantity = 0
+  for (const [_, quantity] of ba.basket.entries()) {
+    totalQuantity += quantity
+  }
+  return totalQuantity
+}
+
+async function renderReviewItems(){
   const container = document.querySelector('.checkout-items-container');
-  // const buyNowBtn = document.querySelector('#buy-btn')
-  let preTotalDOM =  document.querySelector('#review-box-pre-total')
-  let totalDOM = document.querySelector('.review-box-total-value')
   let total = 0;
   const products = await fjs.fetchAllSingles();
   for (const [itemID, quantity] of ba.basket.entries()) {
@@ -26,20 +46,27 @@ export async function renderCheckout(){
     const itemTemplate = t1.content.cloneNode(true);
     const img = itemTemplate.querySelector('#checkout-item-img');
     const itemName = itemTemplate.querySelector('#checkout-item-name');
-    const itemPrice = itemTemplate.querySelector('#checkout-item-value');
+    const itemPriceDOM = itemTemplate.querySelector('#checkout-item-price');
     const checkoutItemDom = itemTemplate.querySelector('.checkout-item');
     const price = product.Price / 100;
-    let itemQuantityDOM = itemTemplate.querySelector('.checkout-item-quantity-value');
+    let itemQuantityDOM = itemTemplate.querySelector('.checkout-item-quantity');
     checkoutItemDom.dataset.id = product.ProductID;
     itemName.textContent = product.ProductName;
     img.src = product.ProductImage;
     img.alt = product.ProductImage;
     itemName.textContent = product.ProductName;
-    itemPrice.textContent = (price).toFixed(2);
-    itemQuantityDOM.textContent = quantity
+    itemPriceDOM.textContent = `£${(price).toFixed(2)}`;
+    itemQuantityDOM.textContent = `${quantity} x`
     total += price * quantity
     container.append(itemTemplate);
   }
+  renderOrderTotal(total)
+}
+
+
+function renderOrderTotal(total){
+  let preTotalDOM =  document.querySelector('#review-box-pre-total')
+  let totalDOM = document.querySelector('.review-box-total-value')
   preTotalDOM.textContent = 'Order Total : £'
   totalDOM.textContent = total.toFixed(2);
 }
@@ -53,7 +80,7 @@ async function init() {
   // await auth.updateAuthUI();
   // await auth.handleAuth0Redirect();
   await ba.initBasket();
-  await renderCheckout();
+  await renderCheckoutPage();
   setupListeners();
 }
 
